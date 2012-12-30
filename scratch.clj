82c9533e83fba9682db5175c2c53a3beb2ad3aa7
nick rothwell <nick@cassiel.com>
2012-12-30 21:55:42 +0000

    Scratch testing.

--- scratch.clj
+++ scratch.clj
@@ -1,7 +1,9 @@
 (ns user
   (:require (cassiel.zeroconf [client :as cl]))
-  (:import [java.net DatagramSocket]
-           [net.loadbang.osc.comms UDPReceiver]))
+  (:import [java.net InetAddress DatagramSocket]
+           [net.loadbang.osc.data Message]
+           [net.loadbang.osc.comms UDPTransmitter UDPReceiver]
+           [net.loadbang.osc.exn CommsException]))
 
 ;; --- checking zeroconf:
 
@@ -27,3 +29,72 @@
 (.open u)
 (.getPort u)
 (.close u)
+
+;; --- Utility printing
+
+(defn pr-message [^Message m]
+  (println (.getAddress m))
+  (doseq [i (range (.getNumArguments m))] (println "->" (.getValue (.getArgument m i))))
+  )
+
+;; --- Create a socket given a consuming function.
+
+(defn start-receiver [f]
+  (let [rx (proxy [UDPReceiver]
+                 []
+               (consumeMessage [date00 m] (f m)))
+        _ (.open rx)]
+
+    (.start (Thread. (reify Runnable
+                       (run [this]
+                         (try
+                           (dorun (repeatedly #(.take rx)))
+                           (catch CommsException _ nil)
+                           )))))
+    rx))
+
+
+
+(defn start-transmitter [a key back-port]
+  (let [{:keys [server port]} (get (cl/examine a) key)
+        tx (UDPTransmitter. (InetAddress/getByName server) port)]
+
+    (.transmit tx
+               (-> (Message. "/sys/port")
+                   (.addInteger back-port)))
+
+    (.transmit tx
+               (-> (Message. "/sys/prefix")
+                   (.addString "/blatt")))
+
+    (.transmit tx
+               (-> (Message. "/sys/info")
+                   (.addString "localhost")
+                   (.addInteger back-port)))
+
+    tx
+    )
+
+  )
+
+(def rx (start-receiver pr-message))
+
+(def tx (start-transmitter a "monome arc 4 (m0000270)" (.getPort rx)))
+
+(.close rx)
+(.close tx)
+
+;; -----
+
+(let [sock (proxy [UDPReceiver]
+               []
+             (consumeMessage [this date00 m]
+               (println m)))
+      {:keys [server port]} (get (cl/examine a) "monome arc 4 (m0000270)")
+      _ (.open sock)
+      in-port (.getPort sock)]
+  (println in-port)
+  (.close sock)
+  )
+
+(get (cl/examine a) "monome arc 4 (m0000270)")
