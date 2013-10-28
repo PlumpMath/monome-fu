6a856eea18ec0337c5285a4e1151cb2b04bb4ccd
nick rothwell <nick@cassiel.com>
2013-10-28 22:19:46 +0000

    zeroconf dependency.

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[eu.cassiel/clojure-zeroconf "1.0.0"]
+  :dependencies [[eu.cassiel/clojure-zeroconf "1.2.0"]
                  [net.loadbang/net.loadbang.osc "1.6.0"]
                  [org.clojure/clojure "1.5.1"]])
